658473088dc52ae78849d07f791a98eab91af521
ap <arsenicpaper9@gmail.com>
2024-02-14 06:35:10 +0300

    commit

--- algorithms/graphs.py
+++ algorithms/graphs.py
@@ -26,41 +26,58 @@ class Graph:
 
 class NewGraph:
 	def __init__(self, verticle_num):
-		self.verticles = [[None for i in range(verticle_num)] for j in range(verticle_num)]
+		self.verticles = [[0 for i in range(verticle_num)] for j in range(verticle_num)]
+		self.colors = list()
 
-	def edge_add(self, v1, v2, value):
-		if v1 == v2:
-			return -1
+	def add_edge(self, v1, v2, value):
+		if v1 == v2 or value == 0:
+			return
 		self.verticles[v1 - 1][v2 - 1] = value
-		self.verticles[v2 - 1][v1- 1] = value
+		self.verticles[v2 - 1][v1 - 1] = value
 	
-	def edge_remove(self, v1, v2):
+	def remove_edge(self, v1, v2):
 		if v1 == v2:
-			return -1
-		self.verticles[v1 - 1][v2 - 1] = None
-		self.verticles[v2 - 1][v1 - 1] = None
+			return
+		self.verticles[v1 - 1][v2 - 1] = 0
+		self.verticles[v2 - 1][v1 - 1] = 0
 	
-	def edge_get(self, v1, v2):
+	def get_edge(self, v1, v2):
 		if v1 == v2:
-			return -1
+			return 0
 		return self.verticles[v1 - 1][v2 - 1]
 		
-	def get_adjacent(self, verticle):
+	def get_adjacents(self, verticle):
 		adjacents = list()
 		path = self.verticles[verticle - 1]
 		for i in range(len(path)):
-			if path[i] is not None:
+			if path[i] != 0:
 				adjacents.append(i + 1)
 		return(adjacents)	
 	
-	def graph_get(self):
+	def get_graph(self):
 		for i in self.verticles:
-			print(i)
+			print(*i)
 
+	def table_overwrite_graph(self, table):
+		for i in range(len(self.verticles)):
+			for j in range(len(self.verticles)):
+				self.add_edge(i + 1, j + 1, int(table[i][j]))
 
-a = NewGraph(5)
-a.edge_add(2, 3, 5)
-a.edge_add(2, 4, 10)
-print(a.edge_get(3, 2))
-print(a.get_adjacent(3))
-a.graph_get()
+	def count_all_edges(self):
+		edge_count = 0
+		for i in range(len(self.verticles)):
+			for j in range(i + 1, len(self.verticles)):
+				if self.verticles[i][j] > 0:
+					edge_count += 1
+		return edge_count
+
+	def set_colors(self, colors):
+		self.colors = [int(color) for color in colors]
+
+	def count_bad_edges(self):
+		bad_edges = 0
+		for i in range(len(self.verticles)):
+			for j in range(i + 1, len(self.verticles)):
+				if self.verticles[i][j] == 1 and self.colors[i] != self.colors[j]:
+					bad_edges += 1
+		return bad_edges
